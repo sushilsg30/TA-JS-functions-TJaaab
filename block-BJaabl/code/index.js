f39c17f64521841acc991f02bae24eb6713df45f
Sushil Ghate <sushilsg30@gmail.com>
2022-02-23 00:03:05 +0530

    block-BJaabl

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,7 +1,10 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
-
+function sayHello(name) {
+  return (alert(`Hello ${name}`));
+}
+sayHello("John");
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,7 +13,11 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
-
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+getFullName("John", "Snow");
+getFullName("Nelson", "Mandels");
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -20,6 +27,11 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+  function addTwoNumbers(a,b) {
+    alert(a + b);
+  }
+  addTwoNumbers(10,22);
+  addTwoNumbers(20,32);
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -32,7 +44,14 @@ calc(10, 20, 'add'); // 30
 calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
-
+function calc (numA, numB, operation) {
+  return (add= numA + numB);
+  return (mul= numA * numB);
+  return (sub= numA + numB);
+}
+calc(10, 20, 'add');
+calc(20, 10, 'sub');
+calc(20, 10, 'mul');
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -41,8 +60,14 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
+function isLeapYear (a,b) {
+  return(true,false);
+}
+isLeapYear(2000,2001);
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function factorial (number){
+  return (factorial);
+}
